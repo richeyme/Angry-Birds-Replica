8515095b890017527de2c277920a3b9d3c2bf6e7
richeyme <richey2@southwestern.edu>
2023-07-21 21:32:25 -0500

    #1 Ball is now draggable
    I moved the script to the specific object that I wanted to attach it to and that seemed to be the issue. It can now be dragged around.

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System; // for the comments
 
 public class Ball : MonoBehaviour {
 
     private bool isPressed;
 
     private Rigidbody2D rb; // make sure that you capitalize correctly
+    private LineRenderer lr; // this will position the ball where ever the mouse is (I think) 
 
+    // this initiatilizes the different parts?
     private void Awake(){
+        Console.WriteLine("Ok ball is seen");
         rb = GetComponent<Rigidbody2D>();
+        lr = GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
@@ -21,6 +26,9 @@ public class Ball : MonoBehaviour {
     }
 
     private void DragBall(){
+        Console.WriteLine("Clicked!"); // trying to see if this is even being used when I click on the ball
+
+        //SetLineRendererPositions(); // here is where the ball matches position of the mouse
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); // Converts the mouse position on screen to a point
         rb.position = mousePosition; // That point is now the point of the object
     }
